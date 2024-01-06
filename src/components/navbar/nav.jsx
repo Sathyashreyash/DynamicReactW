2e4325d8c7935cf6c813e8e0d637b513d22c0566
Sathyashreyash <ashish.natural192@gmail.com>
2024-01-06 20:56:22 +0530

    landiing page is done

---
+++ src/components/navbar/nav.jsx
@@ -0,0 +1,17 @@
+import "./nav.css";
+
+export const nav = () => {
+  return (
+    <div className="nav">
+        <div className="nav-logo">EV-Dynamic</div>
+        <ul className="nav-menu">
+          <li>Home</li>
+          <li>Explore</li>
+          <li>About</li>
+          <li className="nav-contact">Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default nav;
